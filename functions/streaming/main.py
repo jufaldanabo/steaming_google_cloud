48803fc58e5d6e3f2309ff3c95a1b22922ac372d
Juan Fernando Aldana <38257267+jufaldanabo@users.noreply.github.com>
2021-03-27 16:22:00 -0500

    actualizacion

--- functions/streaming/main.py
+++ functions/streaming/main.py
@@ -39,7 +39,7 @@ import pytz
 
 PROJECT_ID = os.getenv('GCP_PROJECT')
 BQ_DATASET = 'talkeydataset'
-BQ_TABLE = 'tk_intelligence'
+BQ_TABLE = 'Talkey_intelligence'
 ERROR_TOPIC = 'projects/%s/topics/%s' % (PROJECT_ID, 'streaming_error_topic')
 SUCCESS_TOPIC = 'projects/%s/topics/%s' % (PROJECT_ID, 'streaming_success_topic')
 DB = firestore.Client()
